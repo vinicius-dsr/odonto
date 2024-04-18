6c32b0ce4b7693e0249c14225b96231a739fc2c9
vinicius-dsr <viniciusreis3105@gmail.com>
2024-04-18 09:13:02 -0300

    alterando header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@
 function Header() {
   return (
     <header className="flex flex-row items-center justify-center py-6">
-      <img src="../../public/header.png" alt="Logo Câncer de Boca" className="md:w-36 w-32"/>
+      <img src="/header.png" alt="Logo Câncer de Boca" className="md:w-36 w-32"/>
     </header>
   )
 }
